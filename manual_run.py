eb1b86456f4939f2f40065c760e9a2a86349e70b
Matic <matic687@gmail.com>
2022-02-09 18:10:53 +0100

    Syntax fix

--- manual_run.py
+++ manual_run.py
@@ -12,9 +12,7 @@ configuration.EPOCHS = 10
 configuration.LEARNING_RATE = 1.0
 configuration.DELTA_CLS_LOSS = 0.01
 configuration.BATCH_SIZE = 1
-configuration.WEIGHTED_SEG_LOSS = True
-configuration.WEIGHTED_SEG_LOSS_P = 2
-configuration.WEIGHTED_SEG_LOSS_MAX = 1
+configuration.WEIGHTED_SEG_LOSS = False
 configuration.DYN_BALANCED_LOSS = True
 configuration.GRADIENT_ADJUSTMENT = True
 configuration.FREQUENCY_SAMPLING = True
@@ -23,7 +21,7 @@ configuration.VALIDATE = True
 configuration.VALIDATE_ON_TEST = False
 configuration.VALIDATION_N_EPOCHS = 5
 configuration.USE_BEST_MODEL = True
-configuration.DICE_THRESHOLD = 1
+configuration.DICE_THRESHOLD = 2
 
 configuration.init_extra()
 
@@ -36,7 +34,5 @@ device = end2end._get_device()
 model = end2end._get_model().to(device)
 end2end.set_dec_gradient_multiplier(model, 0.0)
 end2end._set_results_path()
-
 end2end.eval(model, device, True, False, False, 0.5)
-
 """
