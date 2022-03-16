149d051bf2868da9d3b886bc64c5a397ffbf0f99
Matic <matic687@gmail.com>
2022-03-16 21:18:43 +0100

    Augmentation fix

--- data/dataset.py
+++ data/dataset.py
@@ -88,9 +88,9 @@ class Dataset(torch.utils.data.Dataset):
         self.counter = self.counter + 1
 
         # Augmentacija
-        if self.cfg.AUGMENTATION and self.kind == 'TRAIN':
-            p1 = 0.5
-            p2 = 0.5
+        if self.cfg.AUGMENTATION and self.kind == 'TRAIN' and is_pos:
+            p1 = 0.75
+            p2 = 0.75
             if torch.rand(1) < p1:
                 # Horizontal flip
                 if torch.rand(1) < p2:
